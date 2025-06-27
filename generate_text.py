50eea5046ca8de38382c29fd6f023644f5f97bb4
Sumit4647 <wizradgsm9967@gmail.com>
2025-06-28 03:13:06 +0530

    fix: ensure mesh selection before FBX export

--- generate_text.py
+++ generate_text.py
@@ -5,12 +5,20 @@ text, fname = argv
 
 bpy.ops.wm.read_factory_settings(use_empty=True)
 
+# Create 3D text
 curve = bpy.data.curves.new(type="FONT", name="FT")
-obj = bpy.data.objects.new("Text", curve)
+curve.body = text
+obj = bpy.data.objects.new("TextObj", curve)
 bpy.context.collection.objects.link(obj)
-obj.data.body = text
 
+# Set object active and convert to mesh
 bpy.context.view_layer.objects.active = obj
+obj.select_set(True)
 bpy.ops.object.convert(target="MESH")
 
-bpy.ops.export_scene.fbx(filepath=fname)
+# Deselect all, select only the mesh
+bpy.ops.object.select_all(action='DESELECT')
+obj.select_set(True)
+
+# Export selected as FBX
+bpy.ops.export_scene.fbx(filepath=fname, use_selection=True)
